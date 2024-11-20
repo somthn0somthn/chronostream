66b48d5058020d90fcff274354e99e276b8544a3
somthn0somthn <lpmaho@gmail.com>
2024-11-20 12:19:07 +0900

    init complete

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -124,7 +124,7 @@ fn process_terminate(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramRe
 #[cfg(test)]
 mod test {
     use super::*;
-    use solana_program::{clock::Epoch, clock::Clock};
+    use solana_program::{clock::Clock, clock::Epoch};
     use std::mem;
 
     pub struct Test;
@@ -139,6 +139,22 @@ mod test {
                 unix_timestamp: 1000,
             }
         }
+
+        pub fn time_warp(seconds_to_advance: i64) -> Clock {
+            let base_timestamp = 1000;
+            Clock {
+                slot: 1000,
+                epoch_start_timestamp: 1,
+                epoch: 1,
+                leader_schedule_epoch: 1,
+                unix_timestamp: base_timestamp + seconds_to_advance,
+            }
+        }
+
+        pub const ONE_HOUR: i64 = 3600;
+        pub const ONE_DAY: i64 = 86400;
+        pub const ONE_WEEK: i64 = 86400 * 7;
+        pub const ONE_MONTH: i64 = 86400 * 30;
     }
 
     #[test]
@@ -189,11 +205,7 @@ mod test {
             Epoch::default(),
         );
 
-        let accounts = vec![
-            stream_account,
-            sender_account, 
-            receiver_account,
-        ];
+        let accounts = vec![stream_account, sender_account, receiver_account];
 
         let init_instr = StreamInstruction::Initialize {
             flow_rate: 100,
@@ -223,16 +235,119 @@ mod test {
         println!("receiver {}", &stream.receiver);
         assert_eq!(stream.receiver, receiver_key);
         println!("start time {}", &stream.start_time);
-        assert_eq!(stream.start_time, 1000);  // Should match our mocked timestamp
+        assert_eq!(stream.start_time, 1000); // Should match our mocked timestamp
     }
 
-    //TODO :: write
     #[test]
-    fn test_termination_sender() {}
+    fn test_termination() {
+        let program_id = Pubkey::default();
+        let sender_key = Pubkey::default();
+        let receiver_key = Pubkey::new_unique();
 
-    //TODO :: write
-    #[test]
-    fn test_termination_receiver() {}
+        let mut stream_lamports = 0;
+        let mut stream_data = vec![0; mem::size_of::<StreamConfig>()];
+        let owner = program_id;
+        let binding = Pubkey::new_unique();
+        let stream_account = AccountInfo::new(
+            &binding,
+            false,
+            true,
+            &mut stream_lamports,
+            &mut stream_data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+
+        let mut sender_lamports = 0;
+        let mut sender_data = vec![];
+        let mut sender_account = AccountInfo::new(
+            &sender_key,
+            true,
+            false,
+            &mut sender_lamports,
+            &mut sender_data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+
+        let mut receiver_lamports = 0;
+        let mut receiver_data = vec![];
+        let mut receiver_account = AccountInfo::new(
+            &receiver_key,
+            false,
+            false,
+            &mut receiver_lamports,
+            &mut receiver_data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+
+        // Initialize the stream
+        {
+            let init_accounts = vec![
+                stream_account.clone(),
+                sender_account.clone(),
+                receiver_account.clone(),
+            ];
+
+            let init_instr = StreamInstruction::Initialize {
+                flow_rate: 100,
+                initial_balance: 1000,
+            };
+
+            solana_program::program_stubs::set_syscall_stubs(Box::new(TestSyscallStubs {
+                clock: Test::get_clock(),
+            }));
+
+            let mut init_data = vec![];
+            init_instr.serialize(&mut init_data).unwrap();
+
+            assert_eq!(
+                process_instruction(&program_id, &init_accounts, &init_data),
+                Ok(())
+            );
+        }
+
+        solana_program::program_stubs::set_syscall_stubs(Box::new(TestSyscallStubs {
+            clock: Test::time_warp(Test::ONE_DAY),
+        }));
+
+        sender_account.is_signer = false;
+        receiver_account.is_signer = true;
+
+        // Terminate the stream
+        {
+            let term_accounts = vec![
+                stream_account.clone(),
+                sender_account.clone(),
+                receiver_account.clone(),
+            ];
+
+            let term_instr = StreamInstruction::Terminate;
+            let mut term_data = vec![];
+            term_instr.serialize(&mut term_data).unwrap();
+
+            assert_eq!(
+                process_instruction(&program_id, &term_accounts, &term_data),
+                Ok(())
+            );
+        }
+
+        let stream = StreamConfig::try_from_slice(&stream_account.data.borrow()).unwrap();
+        println!("flow rate term {}", &stream.flow_rate);
+        assert_eq!(stream.flow_rate, 100);
+        println!("static balance term {}", &stream.static_balance);
+        assert_eq!(stream.static_balance, 0);
+        println!("sender term {}", &stream.sender);
+        assert_eq!(stream.sender, sender_key);
+        println!("receiver term {}", &stream.receiver);
+        assert_eq!(stream.receiver, receiver_key);
+        println!("start term time {}", &stream.start_time);
+        assert_eq!(stream.start_time, 1000);
+    }
 }
 
 #[cfg(test)]
@@ -249,3 +364,5 @@ impl solana_program::program_stubs::SyscallStubs for TestSyscallStubs {
         0
     }
 }
+
+//TODO :: Add frontend end testing suite
